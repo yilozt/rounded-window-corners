b86dc68ca3a0c34423a3c54798c834793fdc4280
LuoYi <langisme@qq.com>
2022-07-28 12:11:40 +0800

    Rename name of extensions to 'Rounded Window Corners'
    - tsconfig.ts: change target of TypeScript to ES2020 to make output more
      clear

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 <div align="center">
-    <h1>Rounded Corners Effect</h1>
+    <h1>Rounded Window Corners</h1>
     <!-- <img src="https://img.shields.io/badge/GET%20From-extensions.gnome.org-4A86CF?style=for-the-badge&logo=Gnome&logoColor=white" style="zoom:90%;"/> -->
     <p><i>A gnome-shell extensions that try to add rounded corners for all windows</i></p>
-
 </div>
 
 ## Features
@@ -11,11 +10,9 @@
 - Black list for applications which draw window decoration itself
 - Custom shadow for rounded corners windows
 - Skip libadwaita / libhandy application
-- Experimental feature: Blur effect with rounded corners
 
 ## Notes
 
-- Need `libadwaita` installed to open preferences pages in Gnome 40/41
 - The rounded corners effect for window is base on this [shader](https://gitlab.gnome.org/GNOME/mutter/-/blob/main/src/compositor/meta-background-content.c#L138) from mutter project
 - The Blur effect with rounded corners mix [Shell.BlurEffect](https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/src/shell-blur-effect.c) and the above shader
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,31 @@
-// extension.js
-// We will load our app.js in this file but nothing else.
+/// extension.js
+/// We will load our app.js in this file but nothing else.
 
-// Load extensions from ./app.js via import(), so that we can
-// try ES modules in our extensions.
-//
-// https://gitlab.gnome.org/GNOME/gjs/-/blob/master/doc/ESModules.md
-const extension = import ('./app.js')
-    .then ((app) => new app.Extension ())
-    .catch ((err) => err_msg ('Failed to load extensions.', err))
+// Should be set to null when extension is disabled
+let extension: Promise<Extension | null> | null = null
 
 export function enable () {
+    // Load extensions from ./app.js via import(), so that we can
+    // try ES modules in our extensions.
+    //
+    // https://gitlab.gnome.org/GNOME/gjs/-/blob/master/doc/ESModules.md
+    extension = import ('./app.js')
+        .then ((app) => new app.Extension ())
+        .catch ((e) => {
+            err_msg ('Failed to load app.js', e)
+            return null
+        })
     extension
-        .then ((ext) => ext?.enable ())
-        .catch ((err) => err_msg ('Failed to enable extension', err))
+        ?.then ((ext) => ext?.enable ())
+        .catch ((e) => err_msg ('Failed to enable extensions', e))
 }
 
 export function disable () {
     extension
-        .then ((ext) => ext?.disable ())
+        ?.then ((ext) => {
+            ext?.disable ()
+            extension = null
+        })
         .catch ((err) => err_msg ('Failed to disable extension', err))
 }
 
@@ -26,6 +34,7 @@ export function init () {
 }
 
 import { log, logError } from '@global'
+import { Extension }     from 'app'
 const err_msg = (msg: string, err: Error) => {
     log ('[Rounded Corners Effect] ' + msg)
     logError (err)
