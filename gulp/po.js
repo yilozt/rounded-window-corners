ec7fc9bc3c24a21205a3fe2cf5463f528524e890
Yi <32430186+yilozt@users.noreply.github.com>
2023-10-15 03:11:43 +0800

    Use gettext to compile .po files

--- gulp/po.js
+++ gulp/po.js
@@ -3,13 +3,12 @@
 const { GettextExtractor, JsExtractors, HtmlExtractors } = require('gettext-extractor');
 const fs = require('fs')
 const { uuid, version } = require('../resources/metadata.json')
-const { series, src, dest } = require('gulp')
-const potomo = require('gulp-potomo');
-const rename = require('gulp-rename');
+const { series } = require('gulp')
 const fg = require('fast-glob');
 const { spawnSync } = require('child_process');
 const colors = require('ansi-colors');
 const { stdout } = require('process');
+const { basename } = require('path')
 
 const gen_pot = async () => {
   let extractor = new GettextExtractor()
@@ -43,8 +42,9 @@ const gen_pot = async () => {
   extractor.printStats()
 }
 
+const cmd = (command, args) => spawnSync(command, args, { shell: true, stdio: 'inherit' })
+
 const fill_pot = async () => {
-  const cmd = (command, args) => spawnSync(command, args, { shell: true, stdio: 'inherit' })
   const pot = (await fg('po/*.pot'))[0]
   const po_files = await fg('po/*.po')
 
@@ -54,15 +54,18 @@ const fill_pot = async () => {
   }
 }
 
-const compile_po = () =>
-    src('po/*.po')
-    .pipe(potomo())
-    .pipe(rename(path => {
-      const locale = path.basename
-      path.dirname += `/locale/${locale}/LC_MESSAGES`
-      path.basename = uuid
-    }))
-    .pipe(dest('_build'))
+const compile_po = async () => {
+  const po_files = await fg('po/*.po');
+  for (const po of po_files) {
+    const locale = basename(po, ".po");
+    const mo_path = `_build/locale/${locale}/LC_MESSAGES`;
+    const mo = `${mo_path}/${uuid}.mo`;
+
+    fs.mkdirSync(mo_path, { recursive: true });
+    cmd('msgfmt', [ '-o', mo, po ]);
+    stdout.write(colors.green(`Generate ${mo}\n`))
+  }
+}
 
 exports.compile_po = compile_po
 exports.po = series(gen_pot, fill_pot, compile_po)
